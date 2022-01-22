ed529a74c66620c08a4827f250e051b53b7e7299
Sagar Mittal <mittalsagar006@gmail.com>
2022-01-22 12:08:05 +0530

    Time: 15 ms (95.72%), Space: 23 MB (99.96%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int l = 0;
         int r = s.size()-1;
         while(l < r) {
-            swap(s[l], s[r]);
-            l++;
-            r--;
+            swap(s[l++], s[r--]);
         }
     }
 };
