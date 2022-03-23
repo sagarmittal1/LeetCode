4f65d19ac2c71c064fbf2611a3a5f98a5bb54c94
Sagar Mittal <mittalsagar006@gmail.com>
2022-03-23 22:59:57 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (54.40%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         int n1 = word1.size(), n2 = word2.size();
-        string ans;
-        
-        for(int i = 0; i < min(n1,n2); i++) {
-            ans.push_back(word1[i]);
-            ans.push_back(word2[i]);
-        }
-        if(n1 != n2) {
-            if(n1 > n2) {
-                for(int i = n2; i < n1; i++) ans.push_back(word1[i]);
-            }else{
-                for(int i = n1; i < n2; i++) ans.push_back(word2[i]);
-            }
+        string ans = "";
+        for(int i = 0; i < max(n1,n2); i++) {
+            if(i < n1) ans.push_back(word1[i]);
+            if(i < n2) ans.push_back(word2[i]);
         }
         return ans;
     }
