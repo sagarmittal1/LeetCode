a6099c6a62925bc78f69c05423c1b56f0fee0cb3
Sagar Mittal <mittalsagar006@gmail.com>
2022-03-25 01:34:17 +0530

    Time: 142 ms (21.50%), Space: 41.9 MB (57.44%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& v, int limit) {
+        sort(v.begin(), v.end());
+        reverse(v.begin(), v.end());
+        
+        int boats = 0;
+        int l = 0, r = v.size()-1;
+        
+        while(l <= r) {
+            if(v[l] + v[r] <= limit) {
+                boats++;
+                l++; r--;
+            }else{
+                boats++;
+                l++;
+            }
+        }
+        return boats;
+        
+    }
+};
