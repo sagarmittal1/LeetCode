7a9e8139bd31cd3748777eca311d546440770187
Sagar Mittal <mittalsagar006@gmail.com>
2022-04-02 18:24:21 +0530

    Added solution - LeetHub

--- 7. Level order traversal - GFG/7.-level-order-traversal.cpp
+++ 7. Level order traversal - GFG/7.-level-order-traversal.cpp
@@ -41,27 +41,24 @@ struct Node
 class Solution
 {
     public:
-    void solve(Node *root, vector <int> &ans) {
-        if(root == NULL) return;
-        queue <Node *> q;
-        q.push(root);
-        
-        while(!q.empty()) {
-            Node *temp = q.front();
-            q.pop();
-            if(temp != NULL) {
-                ans.push_back(temp->data);
-                q.push(temp->left);
-                q.push(temp->right);
-            }
-            
-        }
-    }
     //Function to return the level order traversal of a tree.
     vector<int> levelOrder(Node* node)
     {
         vector <int> ans;
-        solve(node, ans);
+        if(node == NULL) return ans;
+        
+        queue <Node *> q;
+        q.push(node);
+        
+        while(!q.empty()) {
+            Node *temp = q.front();
+            q.pop();
+            ans.push_back(temp->data);
+            if(temp->left != NULL) {
+                q.push(temp->left);
+            }
+            if(temp->right != NULL) q.push(temp->right);
+        }
         return ans;
     }
 };
