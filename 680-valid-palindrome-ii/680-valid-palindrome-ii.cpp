4416c5b12991236f40b072d7fd3e274c45bbc76c
Sagar Mittal <mittalsagar006@gmail.com>
2022-04-02 16:21:16 +0530

    Time: 71 ms (46.54%), Space: 24 MB (39.29%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
+    bool isPalin(string s, int x, int y) {
+        int l = x, r = y;
+        while(l < r) {
+            if(s[l] != s[r]) return false;
+            l++; r--;
+        }
+        return true;
+    }
+    
     bool validPalindrome(string s) {
         int l = 0, r = s.size()-1;
-        int ans = 0;
         
         while(l < r) {
-            if(s[l] != s[r]) {
-                l++;
-                ans++;
-            }else{
+            if(s[l] == s[r]) {
                 l++; r--;
+            }else{
+                return isPalin(s, l+1, r) or isPalin(s, l, r-1);
             }
         }
-        
-        if(ans <= 1) return true;
-        
-        l = 0, r = s.size()-1, ans = 0;
-        while(l < r) {
-            if(s[l] != s[r]) {
-                r--;
-                ans++;
-            }else{
-                l++; r--;
-            }
-        }
-        
-        if(ans <= 1) return true;
-        return false;
-        
+        return true;
     }
 };
